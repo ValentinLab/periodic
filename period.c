01280c3399a5c004fefdbd273dc6981ea2555b69
Valentin Perignon <valentin.perignon@orange.fr>
2020-04-23 15:27:32 +0200

    Écriture du PID

--- period.c
+++ period.c
@@ -1,16 +1,28 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include "controlstream.h"
 
 void write_pid() {
+  // PID filename
+  char *pid_filename = "/tmp/period.pid";
+
   // Check if PID file already exists
-  FILE *f = fopen_control_errors("text.txt", "w");
-  fprintf(f, "coucou");
-  fclose_control_errors(f);
+  int acc = access(pid_filename, F_OK);
+  if(acc == 0) {
+    fprintf(stderr, "Error: a period process is already running.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Write pid
+  FILE *pid_output = fopen_control_errors(pid_filename, "w");
+  fprintf(pid_output, "%d", getpid());
+  fclose_control_errors(pid_output);
 }
 
 int main(int argc, char **argv) {
-  // TODO : question 6.2 (pour la version finale)
   write_pid();
+  // TODO : question 6.2 (pour la version finale)
+  
   return 0;
 }
