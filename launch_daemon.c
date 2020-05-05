194876d4efc93c44629bd970b3379f763b899958
Valentin Perignon <valentin.perignon@orange.fr>
2020-05-05 15:02:05 +0200

    Launch_daemo

--- launch_daemon.c
+++ launch_daemon.c
@@ -3,10 +3,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <unistd.h>
 #include "controlsyscall.h"
 
-#define USAGE "launch_daemon ABSOLUTE_PATH"
+#define USAGE "launch_daemon ABSOLUTE_PATH\n"
 
 int main(int argc, char **argv) {
   // Check arguments number
@@ -15,7 +16,21 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
   // Check path to daemon
-  
+  const char *path_to_daemon = argv[1];
+  struct stat st_daemon;
+  int ret = stat(path_to_daemon, &st_daemon);
+  if(ret == -1) {
+    if(errno == ENOENT) {
+      fprintf(stderr, "Error: no such file or directory\n%s", USAGE);
+      return EXIT_FAILURE;
+    }
+    perror("Get informations about the daemon (stat)");
+    return EXIT_SYSCALL;
+  }
+  if(!S_ISREG(st_daemon.st_mode)) {
+    fprintf(stderr, "Error: the file must be a regular file\n%s", USAGE);
+    return EXIT_FAILURE;
+  }
 
   // Fork
   pid_t pid = fork_control();
@@ -42,7 +57,7 @@ int main(int argc, char **argv) {
       close_control(2);
 
       // Exec daemon
-      execl("DAEMON_PATHNAME", "period", NULL);
+      execl(path_to_daemon, "period", NULL);
       perror("Launch period (execl)");
       exit(EXIT_SYSCALL);
     }
