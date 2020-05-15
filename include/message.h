2b938318ae5a0c6e7b8ba49b8de49f4bb642ddfa
Valentin Perignon <valentin.perignon@orange.fr>
2020-05-15 12:22:33 +0200

    On avance

--- include/message.h
+++ include/message.h
@@ -6,14 +6,14 @@
  * 
  * @param fd Le descripteur de fichier
  * @param str La chaine que l'on souhaite envoyer
- */ 
+ */
 int send_string(int fd, const char *str);
 
 /**
  * Permet de recevoir une chaine de caractére via un descripteur de fichier
  * 
  * @param fd Le descripteur de fichier
- */ 
+ */
 char *recv_string(int fd);
 
 /**
@@ -21,14 +21,14 @@ char *recv_string(int fd);
  * 
  * @param fd Le descripteur de fichier
  * @param argv Le tableau de chaine de caractére
- */ 
+ */
 int send_argv(int fd, char *argv[]);
 
 /**
  * Permet de recevoir un tableau de chaine de caractére via un descripteur de fichier
  * 
  * @param fd Le descripteur de fichier
- */ 
+ */
 char **recv_argv(int fd);
 
 #endif
