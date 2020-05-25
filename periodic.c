0f6586cb54f3567ab05f7bef754c0df16960bc46
EvoXNeo <evox.neo@gmail.com>
2020-05-25 14:11:37 +0200

    change read all comand

--- periodic.c
+++ periodic.c
@@ -61,24 +61,24 @@ int main(int argc, char *argv[]) {
     perror_control(send_signal(pid, SIGUSR2), "Can't send SIGUSR2 (periodic)");
     printf("Registred command : \n");
     char **res = recv_argv(fd);
-    close(fd);
-    while (res[0] != NULL) {
+    while (res != NULL) {
       int i = 0;
-      while (res[i]) {
-        printf("%s", res[i]);
+      while (res[i] != NULL) {
+        printf("%s ", res[i]);
         i++;
       }
       printf("\n");
       res = recv_argv(fd);
     }
+    close(fd);
   } else {  // Si il y a 3 arguments au moins
     perror_control(send_signal(pid, SIGUSR1), "Can't send SIGUSR1 (periodic)");
 
     int fd = open(NAMED_PIPE_PATH, O_WRONLY);
     perror_control(fd, "open named pipe (periodic)");
     char numArg[12];
-    sprintf(numArg, "%d", argc - 4);
-    send_string(fd, numArg);
+    // sprintf(numArg, "%d", argc - 4);
+    // send_string(fd, numArg);
     send_argv(fd, argv+1);
     close(fd);
   }
