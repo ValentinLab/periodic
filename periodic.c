b191297341b7b082c1e555a46b4a4d614c184c69
EvoXNeo <evox.neo@gmail.com>
2020-05-25 19:22:43 +0200

    change a lot of thing

--- periodic.c
+++ periodic.c
@@ -14,7 +14,7 @@
 
 #define PID_PATH "/tmp/period.pid"
 #define NAMED_PIPE_PATH "/tmp/period.fifo"
-#define MAX_PID_SIZE 4
+#define GENERIC_USAGE "usage : ./periodic start period cmd [args]...\nusage : ./periodic cmd_number\nusage : ./periodic\n"
 
 /**
  * Permet de lire le pid dans un fichier
@@ -23,17 +23,15 @@
  * @return Renvoie le pid ou -1 si le fichier n'existe pas
  */ 
 int read_pid(char *path) {
-  char buf[MAX_PID_SIZE];
   FILE* file = fopen_control(path, "r");
+  char pid[12];
 
-  while (!feof(file)) {
-    fread(buf,sizeof(char),MAX_PID_SIZE,file);
-  }
-  
-  printf("pid of period : %s\n",buf);
+  fgets(pid, 12, file);
   fclose_control(file);
 
-  return atoi(buf);
+  printf("pid of period : %s\n", pid);
+  
+  return atoi(pid);
 }
 
 /**
@@ -45,9 +43,21 @@ int send_signal (int pid, int signal) {
   return res;
 }
 
+long is_long(char *str, char *err) {
+  char **endptr = calloc(strlen(str), sizeof(char *));
+  long result = strtol(str, endptr, 10);
+  if(strcmp(*endptr, "\0") != 0) {
+    fprintf(stderr,"invalid %s\n%s", err, GENERIC_USAGE);
+    free(endptr);
+    exit(EXIT_FAILURE);
+  }
+  free(endptr);
+  return result;
+}
+
 int main(int argc, char *argv[]) {
-  if (argc != 1 && argc <= 3) {
-    fprintf(stderr,"invalid arguments\nusage : ./periodic start period cmd [args]...\nusage : ./periodic\n");
+  if (argc <= 0 || argc == 3) {
+    fprintf(stderr,"invalid arguments\n%s", GENERIC_USAGE);
     return EXIT_FAILURE;
   }
   
@@ -57,7 +67,7 @@ int main(int argc, char *argv[]) {
 
   if (argc == 1) { // Si il y a 0 argument
     int fd = open(NAMED_PIPE_PATH, O_RDWR);
-    perror_control(fd, "open named pipe (periodic)");
+    perror_control(fd, "open (periodic - 1)");
 
     perror_control(send_signal(pid, SIGUSR2), "Can't send SIGUSR2 (periodic)");
     char **res = recv_argv(fd);
@@ -80,45 +90,53 @@ int main(int argc, char *argv[]) {
           free(res[i]);
           i++;
         }
-        printf("\n");
         free(res[i]);
         free(res);
+        printf("\n");
         res = recv_argv(fd);
       }
       free(res[0]);
       free(res);
     }
     close_control(fd);
-  } else {  // Si il y a 3 arguments au moins
-    char **test_start = calloc(strlen(argv[1]), sizeof(char*));
-    char **test_period = calloc(strlen(argv[2]), sizeof(char *));
-    
-    if(strcmp(argv[1], "now") == 0) {
-      time_t now = time(NULL);
-      perror_control(now, "Get time (time)");
-      sprintf(argv[1], "%ld", now);
-    } else{
-      strtol(argv[1], test_start, 10);
-      if(strcmp(*test_start, "\0") != 0) {
-        fprintf(stderr,"invalid start\nusage : ./periodic start period cmd [args]...\nusage : ./periodic\n");
-        free(test_start);
-        free(test_period);
-        exit(EXIT_FAILURE);
-      }
-    }
-    strtol(argv[2], test_period, 10);
-    if(strcmp(*test_period, "\0") != 0) {
-      fprintf(stderr,"invalid period\nusage : ./periodic start period cmd [args]...\nusage : ./periodic\n");
-      free(test_start);
-      free(test_period);
+  } else if(argc == 2) { // Si il y a 1 argument
+    long delete_num = is_long(argv[1], "cmd_number");
+
+    if(delete_num < 0) {
+      fprintf(stderr,"cmd_number need to be superior or equal to zero\n%s", GENERIC_USAGE);
       exit(EXIT_FAILURE);
     }
 
-    perror_control(send_signal(pid, SIGUSR1), "Can't send SIGUSR1 (periodic)");
+    perror_control(send_signal(pid, SIGUSR1), "Can't send SIGUSR1 (periodic - 1)");
 
     int fd = open(NAMED_PIPE_PATH, O_WRONLY);
-    perror_control(fd, "open named pipe (periodic)");
+    perror_control(fd, "open (periodic - 2)");
 
+    // Define the period in delete mod
+    send_string(fd, "1");
+    sprintf(argv[1],"%ld",delete_num);
+    // Send the cmd num that need to be deleted
+    send_string(fd, argv[1]);
+    close_control(fd);
+  } else {  // Si il y a 3 arguments au moins
+    if(strcmp(argv[1], "now") == 0) {
+      time_t now = time(NULL);
+      sprintf(argv[1], "%ld", now);
+    } else{
+      long start = is_long(argv[1], "start");
+      time_t next_time = time(NULL) + start;
+      printf("%ld", next_time);
+      sprintf(argv[1], "%ld", next_time);
+    }
+
+    is_long(argv[2], "period");
+
+    perror_control(send_signal(pid, SIGUSR1), "Can't send SIGUSR1 (periodic - 2)");
+
+    int fd = open(NAMED_PIPE_PATH, O_WRONLY);
+    perror_control(fd, "open (periodic - 3)");
+
+    // Define the period in add mod
     send_string(fd, "0");
 
     // Send argument size
@@ -128,8 +146,6 @@ int main(int argc, char *argv[]) {
     send_string(fd, numArg);
     send_argv(fd, argv+1);
 
-    free(test_start);
-    free(test_period);
     close_control(fd);
   }
 
